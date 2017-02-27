93c76ffdabd53dda588cd1ce44caa9718551295f
1SAVICZOR <1SAVICZOR@GWS03928.ihdsb.CA>
2017-02-27 14:43:39 -0500

    Made Chess board in isSafeAlgorithm.java in Chess

---
+++ Projects2/src/Chess/isSafeAlgorithm.java
@@ -0,0 +1,43 @@
+/*
+Author: Zoran Savic
+Date: 27/02/2017
+Description: Determines wether it is safe to move in chess.
+ */
+package Chess;
+
+/**
+ *
+ * @author 1SAVICZOR
+ */
+public class isSafeAlgorithm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[][] board = new int[8][8];
+        /*
+        pawn
+        rook
+        bishop
+        horse
+        queen
+        king
+        */
+        /*
+        [X]|[0][1][2][3][4][5][6][7]|[X]
+        --------------------------------
+        [7]|[r][h][b][q][k][b][h][r]|[7]
+        [6]|[p][p][p][p][p][p][p][p]|[6]
+        [5]|[ ][ ][ ][ ][ ][ ][ ][ ]|[5]
+        [4]|[ ][ ][ ][ ][ ][ ][ ][ ]|[4]
+        [3]|[ ][ ][ ][ ][ ][ ][ ][ ]|[3]
+        [2]|[ ][ ][ ][ ][ ][ ][ ][ ]|[2]
+        [1]|[p][p][p][p][p][p][p][p]|[1]
+        [0]|[r][h][b][q][k][b][h][r]|[0]
+        --------------------------------
+        [X]|[0][1][2][3][4][5][6][7]|[X]
+        */
+    }
+    
+}
--- Projects2/src/First.java
+++ Projects2/src/First.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package projects2;
+
 
 /**
  *
  * @author 1SAVICZOR
  */
-public class Projects2 {
+public class First {
 
     /**
      * @param args the command line arguments
